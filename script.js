4c60b30e6a5836286df782d70b3633385039a416
Syeda Fatima Jilani <fjilani530@gmail.com>
2026-04-04 07:02:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -84,3 +84,21 @@ function typeEffect() {
 }
 
 typeEffect();
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+// Show button after scrolling down
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+// Scroll to top on click
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
